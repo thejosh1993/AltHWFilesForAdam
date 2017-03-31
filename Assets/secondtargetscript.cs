83fe1b5cccb760d0990dd68d0b6acf64ba3f6c7b
thejosh1993 <josh.davenport@wsu.edu>
2017-03-31 03:10:50 -0700

    DIRECTORY_CHANGE
    I'm not sure if you were able to access my homework yet Adam, so I'm
    going to create a clone in an alternative location and see if that helps
    the issue.

---
+++ Assets/secondtargetscript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondtargetscript : MonoBehaviour {
+    private Renderer rend;
+    public float endurance = 100;
+    public float damage = 25;
+    //amount of health & damage taken per collision 
+    AudioSource audio;
+    private Vector3 position;
+    public GameObject bridge;
+
+    void Start()
+    {
+       audio = GetComponent<AudioSource>();
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name != "impact")
+        {
+            endurance = endurance - damage;
+            audio.Play();
+            transform.localScale += new Vector3(-1, 0, -1);
+            //decrease the size of the target
+        }
+        if (endurance < 0)
+        {
+            Destroy(gameObject);
+            //bridge.transform.position += new Vector3(0, 14, 0);
+            //upon reaching 0, target is destroyed, and bridge is raised.
+
+
+
+
+
+        }
+    }
+}
